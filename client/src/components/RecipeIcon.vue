1b44af67c17890cc9a79ac6496f3d1a0cae2ec3e
Kristijonas <kristi.sr@hotmail.com>
2024-01-29 21:36:29 +0200

    Initial ItemIcon implementation

---
+++ client/src/components/RecipeIcon.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import type { PropType } from 'vue'
+import { RecipeItems } from '@/consts'
+
+const props = defineProps({
+  // NOT A TEST!!!!!
+  itemType: { type: String as PropType<keyof typeof RecipeItems>, required: true },
+})
+
+const pathToIcon = `src/assets/${props.itemType}.svg`
+</script>
+
+<template>
+  <div class="item-icon" draggable="true" :style="{ backgroundImage: `url(${pathToIcon})` }"></div>
+</template>
+
+<style scoped>
+.item-icon {
+  height: 50px;
+  width: 50px;
+  border: 1px solid black;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+</style>
---
+++ client/src/views/CreateRecipeView.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import RecipeIcon from '@/components/RecipeIcon.vue'
+</script>
+
+<template>
+  <div class="admin">
+    <div class="sidebar">
+      <RecipeIcon itemType="image" />
+      <RecipeIcon itemType="description" />
+    </div>
+    <div class="dropzone"></div>
+  </div>
+</template>
+
+<style scoped>
+.admin {
+  display: grid;
+  grid-gap: 10px;
+  grid-template-columns: 300px 300px;
+  grid-template-areas: 'sidebar drop-zone';
+}
+
+.sidebar {
+  grid-area: sidebar;
+  height: 100vh;
+  border: 2px solid red;
+  padding: 20px;
+}
+
+.dropzone {
+  grid-area: drop-zone;
+  height: 100vh;
+  column-gap: 10px;
+}
+</style>
