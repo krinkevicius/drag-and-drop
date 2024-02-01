bed1b3755ae0e83d511c9d9158b68290bc041f24
Kristijonas <kristi.sr@hotmail.com>
2024-02-01 21:01:18 +0200

    Made RecipeItem Draggable

--- client/src/components/RecipeItem.vue
+++ client/src/components/RecipeItem.vue
@@ -2,16 +2,23 @@
 import type { ItemRecord } from '@/stores/createRecipe'
 import type { PropType } from 'vue'
 import { RecipeItems } from '@/consts'
+import { ref } from 'vue'
 
 const props = defineProps({
   item: { type: Object as PropType<ItemRecord>, required: true },
 })
+
+const isDraggable = ref(true)
+
+function toggleDrag() {
+  isDraggable.value = !isDraggable.value
+}
 </script>
 
 <template>
-  <div class="wrapper">
+  <div class="wrapper" :draggable="isDraggable">
     This is a single recipe item
-    <div class="insert">
+    <div class="insert" @mouseenter="toggleDrag" @mouseleave="toggleDrag">
       <component :is="RecipeItems[props.item.componentType].component"></component>
     </div>
   </div>
