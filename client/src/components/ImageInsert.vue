be3f840f53c5d2a446ba68236f7b6f0bc6836bea
Kristijonas <kristi.sr@hotmail.com>
2024-02-11 11:10:53 +0200

    Updated ImageInsert component

--- client/src/components/ImageInsert.vue
+++ client/src/components/ImageInsert.vue
@@ -1,9 +1,38 @@
 <script setup lang="ts">
-// Add script
+import { ref } from 'vue'
+import { useCreateRecipeStore } from '../stores/createRecipe'
+
+const store = useCreateRecipeStore()
+
+const props = defineProps({
+  id: { type: String, required: true },
+})
+const item = store.recipeItems.find((item) => item.id === props.id)
+
+const imageInput = ref<HTMLInputElement | null>(null)
+const image = ref<File | null>(null)
+const imageUrl = ref<string | null>(null)
+
+function addImage() {
+  image.value =
+    imageInput.value && imageInput.value.files?.length ? imageInput.value.files[0] : null
+
+  imageUrl.value = image.value ? URL.createObjectURL(image.value) : null
+
+  item!.data.image = image.value
+  item!.data.imageName = image.value ? image.value.name : ''
+}
 </script>
 
 <template>
-  <div class="image-insert">This is an image</div>
+  <div class="image-insert">
+    This is an image
+    <div v-if="image">
+      Image is added!
+      <img :src="imageUrl!" />
+    </div>
+    <input type="file" @change="addImage" ref="imageInput" accept="image/*" />
+  </div>
 </template>
 
 <style scoped>
--- client/src/consts.ts
+++ client/src/consts.ts
@@ -5,7 +5,7 @@ import CategoryInsert from './components/CategoryInsert.vue'
 
 export const RecipeItems = {
   help: { component: null, data: {} },
-  image: { component: ImageInsert, data: {} },
+  image: { component: ImageInsert, data: { image: null, imageName: '' } },
   description: { component: DescriptionInsert, data: { description: '' } },
   list: { component: ListInsert, data: {} },
   categories: { component: CategoryInsert, data: {} },
