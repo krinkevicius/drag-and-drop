27762aec9944bcef2277770a1806c3a139c5e670
Kristijonas <kristi.sr@hotmail.com>
2024-03-02 14:56:36 +0200

    Added types shared from server to client

---
+++ server/src/shared/entities.ts
@@ -0,0 +1,7 @@
+// Only sharing the types, not the entities themselves!
+// Types are stripped out by the compiler, so this is safe.
+export type * from '../entities/recipe'
+export type * from '../entities/description'
+export type * from '../entities/user'
+export type * from '../entities/image'
+export type * from '../entities/ingredient'
