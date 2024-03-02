637fe7c47ef1bd2470723702d8764c3c58f252e5
Kristijonas <kristi.sr@hotmail.com>
2024-03-02 14:39:13 +0200

    Added more functions to generate fake data for tests

--- server/src/entities/tests/fakes.ts
+++ server/src/entities/tests/fakes.ts
@@ -1,11 +1,13 @@
 import { Chance } from 'chance'
-import { User, Recipe, Description } from '..'
+import { User, Recipe, Description, Image, Ingredient, Category } from '..'
 import { UserRoles } from '../user'
 
 const chance = Chance()
 
+const randomInteger = chance.integer({ min: 1, max: 2147483647 })
+
 export const fakeUser = <T extends Partial<User>>(overrides: T = {} as T) => ({
-  id: chance.integer({ min: 1, max: 2147483647 }),
+  id: randomInteger,
   email: chance.email(),
   username: chance.string({ alpha: true, numeric: true, length: 5 }),
   role: UserRoles.RegistererUser,
@@ -17,9 +19,19 @@ export const fakeUser = <T extends Partial<User>>(overrides: T = {} as T) => ({
 export const fakeRecipe = <T extends Partial<Recipe>>(
   overrides: T = {} as T
 ) => ({
-  id: chance.integer({ min: 1, max: 2147483647 }),
+  id: randomInteger,
   name: chance.word(),
   itemIds: Array.from({ length: 2 }, () => chance.guid({ version: 4 })),
+  ...overrides,
+})
+
+export const fakeRecipeInput = <T extends Partial<Recipe>>(
+  overrides: T = {} as T
+) => ({
+  name: chance.word(),
+  items: Array.from({ length: 2 }, () => ({
+    id: chance.guid({ version: 4 }),
+  })),
   ...overrides,
 })
 
@@ -28,6 +40,32 @@ export const fakeDescription = <T extends Partial<Description>>(
 ) => ({
   id: chance.guid({ version: 4 }),
   descriptionText: `<p>${chance.word()}</p>`,
-  recipeId: chance.integer({ min: 1, max: 2147483647 }),
+  recipeId: randomInteger,
+  ...overrides,
+})
+
+export const fakeImage = <T extends Partial<Image>>(
+  overrides: T = {} as T
+) => ({
+  id: chance.guid({ version: 4 }),
+  imageName: chance.word(),
+  recipeId: randomInteger,
+  imageUrl: `amazonaws.com/${chance.guid({ version: 4 })}`,
+  ...overrides,
+})
+
+export const fakeIngredient = <T extends Partial<Ingredient>>(
+  overrides: T = {} as T
+) => ({
+  id: randomInteger,
+  name: chance.word(),
+  ...overrides,
+})
+
+export const fakeCategory = <T extends Partial<Category>>(
+  overrides: T = {} as T
+) => ({
+  id: randomInteger,
+  name: chance.word(),
   ...overrides,
 })
