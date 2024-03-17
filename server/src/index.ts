54e79e9f52c5d7e17236bb5578830599975cf93e
Kristijonas <kristi.sr@hotmail.com>
2024-03-17 16:35:21 +0200

    Added pino-pretty to format logger messages

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,14 +2,15 @@ import type { DataSourceOptions } from 'typeorm'
 import createApp from './app'
 import { createDatabase } from './database'
 import config from './config'
+import { logger } from './logger'
 
 const database = createDatabase(config.database as DataSourceOptions)
-
 database.initialize().then(() => {
   const app = createApp(database)
 
   app.listen(config.port, () => {
-    // eslint-disable-next-line no-console
-    console.log(`Server is running at http://localhost:${config.port}`)
+    logger.info(
+      `Logger says server is running at http://localhost:${config.port}`
+    )
   })
 })
--- server/src/logger.ts
+++ server/src/logger.ts
@@ -2,5 +2,11 @@ import pino from 'pino'
 import config from './config'
 
 export const logger = pino({
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      ignore: 'pid,hostname',
+    },
+  },
   level: config.env === 'test' ? 'silent' : 'trace',
 })
