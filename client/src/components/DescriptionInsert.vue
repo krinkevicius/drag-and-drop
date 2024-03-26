33c970356492b5dd9ae075f91b96239832e51334
Kristijonas <kristi.sr@hotmail.com>
2024-03-26 15:48:00 +0200

    Updated Description Insert Component

--- client/src/components/DescriptionInsert.vue
+++ client/src/components/DescriptionInsert.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { useCreateRecipeStore } from '../stores/createRecipe'
+import type { DescriptionItem } from '@mono/server/src/shared/entities'
 import { QuillEditor } from '@vueup/vue-quill'
 import '@vueup/vue-quill/dist/vue-quill.snow.css'
 
@@ -9,12 +10,26 @@ const props = defineProps({
   id: { type: String, required: true },
 })
 
-const item = store.recipeItems.find((item) => item.id === props.id)
+const item = store.recipeItems.find((item): item is DescriptionItem => item.id === props.id)
+
+// Possible to add other toolbar options https://quilljs.com/docs/modules/toolbar/
+const toolbarOptions = [
+  ['bold', 'italic', 'underline'],
+
+  [{ list: 'ordered' }],
+
+  [{ header: [1, 2, 3, false] }],
+]
 </script>
 
 <template>
   <div class="description-insert">
-    <QuillEditor theme="snow" v-model:content="item!.data.description" contentType="html" />
+    <QuillEditor
+      theme="snow"
+      :toolbar="toolbarOptions"
+      v-model:content="item!.data.descriptionText"
+      contentType="html"
+    />
   </div>
 </template>
 
