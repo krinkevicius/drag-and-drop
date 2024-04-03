edc9d367f848351203f31c3aff55e48ea9c64308
Kristijonas <kristi.sr@hotmail.com>
2024-04-03 12:05:04 +0300

    Updated tailwind config with color scheme

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,17 +3,20 @@ import flowbitePlugin from 'flowbite/plugin'
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
 
     // .. for monorepo
     '../node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx}',
-    '../node_modules/flowbite/**/*.{js,jsx,ts,tsx}'
+    '../node_modules/flowbite/**/*.{js,jsx,ts,tsx}',
   ],
-  plugins: [
-    flowbitePlugin,
-  ],
+  plugins: [flowbitePlugin],
   theme: {
+    colors: {
+      'main-blue': '#262e5d',
+      'secondary-blue': '#364285',
+      'disabled-blue': '#aeb5de',
+    },
     fontFamily: {
       body: ['"Inter var"', '"Inter"', '"Open Sans"', 'Helvetica', 'Arial', 'sans-serif'],
       mono: ['"Fira Code"', 'monospace'],
