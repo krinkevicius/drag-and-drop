5174c0f40a6b4166d7ccc0bfc705a615eff0b42a
Kristijonas <kristi.sr@hotmail.com>
2024-04-04 14:44:03 +0300

    Updated with tailwind classes

--- client/src/components/Popup.vue
+++ client/src/components/Popup.vue
@@ -1,39 +1,26 @@
 <script setup lang="ts">
-defineProps<{
-  open: boolean
-  close: Function
-}>()
+defineProps({
+  open: { type: Boolean, required: true },
+  close: { type: Function, required: true },
+  zIndex: { type: Number, default: 95 },
+})
 </script>
 
 <template>
-  <div class="popup" v-if="open">
-    <div class="inner-popup">
-      <button @click="close()">Close Popup</button>
-      <slot name="popupContent"></slot>
+  <div
+    class="fixed inset-0 flex items-center justify-center bg-[rgb(0_0_0_/_20%)]"
+    :style="{ zIndex: zIndex }"
+    v-if="open"
+  >
+    <div
+      class="mb-4 max-w-xs rounded-xl border-4 border-secondary-blue bg-white pb-8 pt-0 shadow-md"
+    >
+      <div class="rounded-t-l flex justify-end self-stretch bg-main-blue p-2.5 text-white">
+        <button class="hover:underline" title="Close popup" @click="close()">X</button>
+      </div>
+      <div class="px-8 py-4 text-center">
+        <slot name="popupContent"></slot>
+      </div>
     </div>
   </div>
 </template>
-
-<style scoped>
-.popup {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: fixed;
-  inset: 0;
-  z-index: 99;
-  background-color: rgb(0 0 0 / 20%);
-}
-
-.inner-popup {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  background-color: white;
-  padding: 32px;
-  border: 5px solid black;
-  border-radius: 30px;
-  max-width: 90%;
-  gap: 5px;
-}
-</style>
