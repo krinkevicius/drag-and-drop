1834fb9b234c3c85846b300e102649ae4c2185b6
Kristijonas <kristi.sr@hotmail.com>
2024-04-04 10:38:59 +0300

    Updated with tailwind classes

--- client/src/components/HeaderComponent.vue
+++ client/src/components/HeaderComponent.vue
@@ -1,33 +1,26 @@
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { ref, toRefs, computed, watch } from 'vue'
 import { useUserStore } from '@/stores/users'
 import type { UserLogin, UserSignup } from '@/stores/users'
+import { useErrorStore } from '@/stores/errorStore'
 import { withError } from '@/composables/useErrorHandling'
-
 import { useRouter } from 'vue-router'
 import AuthenticatedContent from '@/components/AuthenticatedContent.vue'
 import AdminContent from '@/components/AdminContent.vue'
+import Popup from '@/components/Popup.vue'
+import FormButton from '@/components/FormButton.vue'
 
 const router = useRouter()
 const userStore = useUserStore()
+const { globalErrorMessage } = toRefs(useErrorStore())
 
-const errorMessage = ref<string>('')
+const loading = ref<boolean>(false)
+const successMessage = ref<string>('')
 
 const loginForm = ref<UserLogin>({
   email: '',
   password: '',
 })
-
-const submitLogin = withError(async () => {
-  await userStore.login(loginForm.value)
-
-  togglePopup()
-  if (userStore.userRole === 'admin') {
-    router.push({ name: 'CreateRecipe' })
-  }
-}, errorMessage)
-
-const signupSuccess = ref<boolean>(false)
 
 const signupForm = ref<UserSignup>({
   email: '',
@@ -35,160 +28,234 @@ const signupForm = ref<UserSignup>({
   password: '',
 })
 
-const submitSignup = withError(async () => {
-  await userStore.signup(signupForm.value)
-  signupSuccess.value = true
-}, errorMessage)
-
-const popup = ref<boolean>(false)
 const loginVisible = ref<boolean>(false)
 const signupVisible = ref<boolean>(false)
 
-function togglePopup(open?: boolean) {
-  errorMessage.value = ''
-  popup.value = !popup.value
-  if (open === undefined) {
-    loginVisible.value = false
-    signupVisible.value = false
-    // signupSuccess reset???
+const login = withError(async () => {
+  loading.value = true
+  await userStore.login(loginForm.value)
+  loginForm.value.email = ''
+  loginForm.value.password = ''
+  loading.value = false
+  loginVisible.value = false
+}, globalErrorMessage)
+
+const signup = withError(async () => {
+  loading.value = true
+  await userStore.signup(signupForm.value)
+  signupForm.value.email = ''
+  signupForm.value.username = ''
+  signupForm.value.password = ''
+  successMessage.value = 'Success! you can login now.'
+  loading.value = false
+}, globalErrorMessage)
+
+watch(globalErrorMessage, () => {
+  if (!globalErrorMessage.value) return
+  loading.value = false
+})
+
+const popupOpen = computed(() => {
+  if (globalErrorMessage.value) {
+    return false
   }
+
+  if (loginVisible.value || signupVisible.value) return true
+
+  return false
+})
+
+function closePopup() {
+  if (loading.value) return
+  loginVisible.value = false
+  signupVisible.value = false
+  successMessage.value = ''
 }
 
-function test() {
-  throw new Error('This is error in front end')
+function switchToLogin() {
+  successMessage.value = ''
+  signupVisible.value = false
+  loginVisible.value = true
 }
 </script>
 
 <template>
-  <button @click="test">Test</button>
-  <div>Welcome to Blue Kitchen!</div>
-  <AuthenticatedContent>
-    <template #authContent>
-      <div>
-        User is logged in
-        <AdminContent>
-          <template #adminContent>
+  <Popup :open="popupOpen" :close="closePopup">
+    <template #popupContent>
+      <div class="w-full max-w-xs">
+        <div v-if="loginVisible">
+          <form>
+            <div class="mb-4">
+              <label class="mb-2 block text-sm font-bold text-gray-700" for="login-email">
+                Email
+              </label>
+              <input
+                class="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
+                id="login-email"
+                type="email"
+                v-model="loginForm.email"
+                placeholder="Email"
+                :required="true"
+              />
+            </div>
+            <div class="mb-4">
+              <label class="mb-2 block text-sm font-bold text-gray-700" for="login-password">
+                Password
+              </label>
+              <input
+                class="focus:shadow-outline mb-3 w-full appearance-none rounded border border-red-500 px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
+                id="login-password"
+                type="password"
+                v-model="loginForm.password"
+                placeholder="Password"
+                :required="true"
+              />
+            </div>
+            <div class="flex justify-center">
+              <FormButton
+                :button-text="'Login'"
+                :loading="loading"
+                :successMessage="successMessage"
+                @click="login"
+              />
+            </div>
+          </form>
+        </div>
+        <div v-if="signupVisible">
+          <form>
+            <div class="mb-4">
+              <label class="mb-2 block text-sm font-bold text-gray-700" for="signup-email">
+                Email
+              </label>
+              <input
+                class="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
+                id="submit-email"
+                type="email"
+                v-model="signupForm.email"
+                placeholder="Email"
+                :required="true"
+              />
+            </div>
+            <div class="mb-4">
+              <label class="mb-2 block text-sm font-bold text-gray-700" for="signup-username">
+                Username
+              </label>
+              <input
+                class="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
+                id="signup-username"
+                type="text"
+                v-model="signupForm.username"
+                placeholder="Username"
+                :required="true"
+              />
+            </div>
+            <div class="mb-4">
+              <label class="mb-2 block text-sm font-bold text-gray-700" for="signup-password">
+                Password
+              </label>
+              <input
+                class="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
+                id="signup-password"
+                type="password"
+                v-model="signupForm.password"
+                placeholder="Password"
+                :required="true"
+              />
+            </div>
+            <div class="flex justify-center">
+              <FormButton
+                :button-text="'Signup'"
+                :loading="loading"
+                :successMessage="successMessage"
+                @click="signup"
+              >
+                <template #onSuccess>
+                  <div class="flex justify-center">
+                    <button
+                      class="focus:shadow-outline rounded bg-main-blue px-4 py-2 font-bold text-white hover:bg-secondary-blue focus:outline-none"
+                      @click="switchToLogin"
+                      type="button"
+                    >
+                      Go to Login
+                    </button>
+                  </div>
+                </template>
+              </FormButton>
+            </div>
+          </form>
+        </div>
+      </div>
+    </template>
+  </Popup>
+  <div class="w-full bg-main-blue px-8 py-8 text-xs text-white md:text-base">
+    <div class="container mx-auto">
+      <div class="flex-none justify-between md:flex">
+        <div class="mb-1 md:mb-0">
+          <router-link to="/" class="hover:underline">Welcome to Blue Kitchen!</router-link>
+        </div>
+        <AuthenticatedContent>
+          <template #authContent>
+            <div class="flex space-x-1">
+              <div>Hello, {{ userStore.username }}!</div>
+              <AdminContent>
+                <template #adminContent>
+                  <div class="flex space-x-1">
+                    <div>|</div>
+                    <button
+                      class="hover:underline"
+                      @click="
+                        () => {
+                          router.push({ name: 'CreateRecipe' })
+                        }
+                      "
+                    >
+                      Dashboard
+                    </button>
+                  </div>
+                </template>
+              </AdminContent>
+              <div>|</div>
+              <button
+                class="hover:underline"
+                @click="
+                  () => {
+                    router.push({ name: 'FavoriteRecipes' })
+                  }
+                "
+              >
+                My favorites
+              </button>
+              <div>|</div>
+              <button class="hover:underline" @click="userStore.logout" data-testid="logoutButton">
+                Log Out
+              </button>
+            </div>
+          </template>
+          <template #default>
             <button
+              class="hover:underline"
               @click="
                 () => {
-                  router.push({ name: 'CreateRecipe' })
+                  loginVisible = true
                 }
               "
             >
-              Dashboard
+              Login
+            </button>
+            |
+            <button
+              class="hover:underline"
+              @click="
+                () => {
+                  signupVisible = true
+                }
+              "
+            >
+              Sign Up
             </button>
           </template>
-        </AdminContent>
-        <button
-          @click="
-            () => {
-              router.push({ name: 'FavoriteRecipes' })
-            }
-          "
-        >
-          My favorites
-        </button>
-        <button @click="userStore.logout" data-testid="logoutButton">Log Out</button>
-      </div>
-    </template>
-    <template #default>
-      <button
-        @click="
-          () => {
-            loginVisible = true
-            togglePopup(true)
-          }
-        "
-      >
-        Login
-      </button>
-      |
-      <button
-        @click="
-          () => {
-            signupVisible = true
-            togglePopup(true)
-          }
-        "
-      >
-        Sign Up
-      </button>
-    </template>
-  </AuthenticatedContent>
-  <div class="popup" v-if="popup">
-    <div class="inner-popup">
-      <button @click="togglePopup()">Close popup</button>
-      <div v-if="loginVisible">
-        <form @submit.prevent="submitLogin">
-          <input type="email" v-model="loginForm.email" placeholder="Email" :required="true" />
-          <input
-            type="password"
-            v-model="loginForm.password"
-            placeholder="Password"
-            :required="true"
-          />
-          <button type="submit">Login</button>
-        </form>
-      </div>
-      <div v-if="signupVisible">
-        <form @submit.prevent="submitSignup">
-          <input type="email" v-model="signupForm.email" placeholder="Email" :required="true" />
-          <input
-            type="text"
-            v-model="signupForm.username"
-            placeholder="Username"
-            :required="true"
-          />
-          <input
-            type="password"
-            v-model="signupForm.password"
-            placeholder="Password"
-            :required="true"
-          />
-          <button type="submit">Signup</button>
-        </form>
-        <div v-if="signupSuccess">
-          Success! You can login now.
-          <button
-            @click="
-              () => {
-                signupVisible = false
-                signupSuccess = false
-                loginVisible = true
-              }
-            "
-          >
-            Go to login
-          </button>
-        </div>
-      </div>
-      <div v-if="errorMessage">
-        {{ errorMessage }}
+        </AuthenticatedContent>
       </div>
     </div>
   </div>
 </template>
-
-<style scoped>
-.popup {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: fixed;
-  inset: 0;
-  z-index: 99;
-  background-color: rgb(0 0 0 / 20%);
-}
-
-.inner-popup {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  background-color: white;
-  padding: 32px;
-  border: 5px solid black;
-  border-radius: 30px;
-  max-width: 90%;
-  gap: 5px;
-}
-</style>
