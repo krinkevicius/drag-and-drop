76a6e93b73cf7327005735ff789bd8530140e440
Kristijonas <kristi.sr@hotmail.com>
2024-04-04 14:41:16 +0300

    Added recipe card

---
+++ client/src/components/RecipeCard.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import type { RecipeForCard } from '@mono/server/src/shared/entities'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const props = defineProps<{
+  recipe: RecipeForCard
+}>()
+
+function goToRecipe() {
+  router.push({ name: 'SingleRecipe', params: { id: props.recipe.id } })
+}
+</script>
+
+<template>
+  <div
+    @click="goToRecipe"
+    class="relative aspect-square overflow-hidden rounded-lg border-2 border-black hover:cursor-pointer"
+  >
+    <img
+      class="max-h-64 w-full bg-white object-cover"
+      :src="recipe.imageUrl || `/logo.svg`"
+      :alt="recipe.name"
+    />
+    <div
+      class="absolute bottom-0 w-full bg-gradient-to-b from-transparent to-black text-center text-white"
+    >
+      <div class="px-4 py-5">
+        <strong class="text-xl font-bold">{{ recipe.name }}</strong>
+      </div>
+    </div>
+  </div>
+</template>
