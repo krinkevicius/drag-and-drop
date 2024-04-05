0d88bef56c78f5379e0ee8f46a91bf1d4f5e4d02
Kristijonas <kristi.sr@hotmail.com>
2024-04-05 19:14:04 +0300

    Updated defaults with components for single recipe viewing

--- client/src/consts.ts
+++ client/src/consts.ts
@@ -1,12 +1,32 @@
 import ImageInsert from '@/components/ImageInsert.vue'
+import SingleRecipeImage from '@/components/singleRecipe/SingleRecipeImage.vue'
 import DescriptionInsert from '@/components/DescriptionInsert.vue'
+import SingleRecipeDescription from '@/components/singleRecipe/SingleRecipeDescription.vue'
 import ListInsert from '@/components/ListInsert.vue'
+import SingleRecipeIngredientList from '@/components/singleRecipe/SingleRecipeIngredientList.vue'
 import CategoryInsert from './components/CategoryInsert.vue'
+import SingleRecipeCategories from '@/components/singleRecipe/SingleRecipeCategories.vue'
 
-export const RecipeItems = {
+export const recipeItemsDefault = {
   help: { component: null, data: {} },
-  image: { component: ImageInsert, data: { image: null, imageName: '' } },
-  description: { component: DescriptionInsert, data: { description: '' } },
-  list: { component: ListInsert, data: {} },
-  categories: { component: CategoryInsert, data: {} },
+  image: {
+    component: ImageInsert,
+    singleRecipeComponent: SingleRecipeImage,
+    data: { imageUrl: '', imageName: '' },
+  },
+  description: {
+    component: DescriptionInsert,
+    singleRecipeComponent: SingleRecipeDescription,
+    data: { descriptionText: '' },
+  },
+  ingredientList: {
+    component: ListInsert,
+    singleRecipeComponent: SingleRecipeIngredientList,
+    data: { quantityIngredientPairs: [] },
+  },
+  categories: {
+    component: CategoryInsert,
+    singleRecipeComponent: SingleRecipeCategories,
+    data: { categories: [] },
+  },
 }
