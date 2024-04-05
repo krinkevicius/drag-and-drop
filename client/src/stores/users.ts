cea35a1132fbba5984654fa5efd71732850f93da
Kristijonas <kristi.sr@hotmail.com>
2024-04-05 19:03:02 +0300

    Updated store to handle favorite recipes

--- client/src/stores/users.ts
+++ client/src/stores/users.ts
@@ -8,35 +8,28 @@ import { defineStore } from 'pinia'
 import type { Ref } from 'vue'
 import { ref, computed } from 'vue'
 import { trpc } from '@/trpc'
-// import { useRouter } from 'vue-router'
-import type { Recipe } from '@mono/server/src/shared/entities'
-import type { UserBare } from '@mono/server/src/shared/entities'
+import { useRouter } from 'vue-router'
+import type { RecipeForCard, UserBare } from '@mono/server/src/shared/entities'
 
 export type UserSignup = Pick<UserBare, 'email' | 'username' | 'password'>
 export type UserLogin = Pick<UserBare, 'email' | 'password'>
 
 export const useUserStore = defineStore('userStore', () => {
-  // const router = useRouter()
-  const userFavorites: Ref<Recipe[]> = ref([])
+  const router = useRouter()
+  const userFavorites: Ref<RecipeForCard[]> = ref([])
   const userToken = ref(getStoredAccessToken(localStorage))
   const isLoggedIn = computed(() => !!userToken.value)
 
   const userRole = computed(() => (userToken.value ? getUserFromToken(userToken.value).role : null))
 
-  /*async function addRemoveFavorites(recipe: Recipe) {
-    const index = userFavorites.value.findIndex((r) => r.id === recipe.id)
+  const username = computed(() =>
+    userToken.value ? getUserFromToken(userToken.value).username : null
+  )
 
-    // Update the store array
-    if (index !== -1) {
-      userFavorites.value.splice(index, 1)
-    } else {
-      userFavorites.value.push(recipe)
-    }
-
-    // Update in database
-    await trpc.user.addRemoveFavorites.mutate(recipe)
+  async function addRemoveFavorites(recipeId: number) {
+    await trpc.user.addRemoveFavorites.mutate({ id: recipeId })
+    userFavorites.value = await trpc.user.getFavorites.query()
   }
-  */
 
   async function login(loginDetails: UserLogin) {
     const { loginToken } = await trpc.user.login.mutate(loginDetails)
@@ -44,22 +37,13 @@ export const useUserStore = defineStore('userStore', () => {
     userToken.value = loginToken
     storeAccessToken(localStorage, loginToken)
 
-    // based on user role, route to dashboard / home page
-    if (userRole.value === 'admin') {
-      console.log('user is admin') // router.push({ name: 'AdminDashboard' })
-    } else {
-      console.log('user is regular')
-      // router.push({ name: 'Home' })
-    }
+    //load favorite recipes:
+    userFavorites.value = await trpc.user.getFavorites.query()
 
-    //   //load favorite recipes:
-    //   // userFavorites.value = await trpc.user.getFavorites.query()
-    // } catch (error) {
-    //   if (!(error instanceof Error)) {
-    //     throw error
-    //   }
-    //   console.error(error.message)
-    // }
+    // route admins to dashboard
+    if (userRole.value === 'admin') {
+      router.push({ name: 'NewRecipe' })
+    }
   }
 
   async function signup(signupDetails: UserSignup) {
@@ -69,10 +53,19 @@ export const useUserStore = defineStore('userStore', () => {
   function logout() {
     userToken.value = null
     clearStoredAccessToken(localStorage)
-
+    router.push({ name: 'Home' })
     // Remove favorites from the store:
-    //   userFavorites.value = []
+    userFavorites.value = []
   }
 
-  return { isLoggedIn, userFavorites, userRole, /*addRemoveFavorites,*/ login, signup, logout }
+  return {
+    isLoggedIn,
+    userFavorites,
+    userRole,
+    username,
+    addRemoveFavorites,
+    login,
+    signup,
+    logout,
+  }
 })
