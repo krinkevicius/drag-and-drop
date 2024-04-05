19f1391b4cc3003eba8f88106124c51e5a835cc1
Kristijonas <kristi.sr@hotmail.com>
2024-04-05 18:59:39 +0300

    Added error formatter to TRPC

--- server/src/trpc/index.ts
+++ server/src/trpc/index.ts
@@ -5,6 +5,7 @@ import type { AuthUser } from '@server/entities/user'
 import SuperJSON from 'superjson'
 import * as Sentry from '@sentry/node'
 import { ExpectedTRPCError } from '@server/utils/expectedTRPCError'
+import { ZodError } from 'zod'
 
 export type Context = {
   db: Database
@@ -19,6 +20,17 @@ const t = initTRPC.context<Context>().create({
   transformer: SuperJSON,
   errorFormatter(opts) {
     const { error, shape } = opts
+    // if Zod Error is about to be sent to Frontend, it means user provided incorrect input
+    // Update the message before sending
+    if (error.cause instanceof ZodError) {
+      return {
+        ...shape,
+        code: 400,
+        message: `Input error! ${error.cause.issues[0].message}`,
+      }
+    }
+    // If message was not expected it will be logged to Sentry
+    // User will receive a generic message
     if (!(error instanceof ExpectedTRPCError)) {
       Sentry.captureException(error)
       return {
