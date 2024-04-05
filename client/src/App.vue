d9b974e4f9e7c4d9693661596cf227a57002cba2
Kristijonas <kristi.sr@hotmail.com>
2024-04-05 19:25:17 +0300

    Added global error handling

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
+import Popup from '@/components/Popup.vue'
 import HeaderComponent from '@/components/HeaderComponent.vue'
 import { RouterView } from 'vue-router'
+import { useErrorStore } from '@/stores/errorStore'
+
+const errorStore = useErrorStore()
 </script>
 
 <template>
-  <HeaderComponent />
-  <RouterView />
+  <Popup :open="errorStore.isError" :close="errorStore.resetError" :zIndex="99">
+    <template #popupContent>
+      <div>ERROR!</div>
+      <div>{{ errorStore.globalErrorMessage }}</div>
+    </template>
+  </Popup>
+  <div>
+    <HeaderComponent />
+    <RouterView />
+  </div>
 </template>
